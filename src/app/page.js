6122e1acf531e9e91eee15c6801bd3a5ba054b45
aimonchan <mr.minaimonchan@gmail.com>
2025-06-25 22:44:36 +0630

    test6

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,9 @@
-// import HeroSection from './(components)/sections/HeroSection';
-// import AboutSection from './(components)/sections/AboutSection';
-// import StatsSection from './(components)/sections/StatsSection';
-// import SkillsSection from './(components)/sections/SkillsSection';
-// import ResumeSection from './(components)/sections/ResumeSection';
-// import PortfolioSection from './(components)/sections/PortfolioSection';
-// import ServicesSection from './(components)/sections/ServicesSection';
-
-// import ContactSection from './(components)/sections/ContactSection';
 
 import dynamic from 'next/dynamic';
 
 // Import other sections normally if they are SSR-safe
 import HeroSection from './(components)/sections/HeroSection';
 import AboutSection from './(components)/sections/AboutSection';
-// import SkillsSection from './(components)/sections/SkillsSection'; // Also check this if it uses problematic libs
-// ... other safe imports ...
-
-// Dynamically import StatsSection and disable SSR for it
-// const DynamicStatsSection = dynamic(
-//   () => import('./(components)/sections/StatsSection'),
-//   {
-//     ssr: false, // This is the key!
-//     loading: () => <p>Loading stats...</p> // Optional: loading state
-//   }
-// );
-
-// ... other section imports ...
 import PortfolioSection from './(components)/sections/PortfolioSection'; // This also uses client libs, might need dynamic import too
 import ServicesSection from './(components)/sections/ServicesSection';
 import TestimonialsSection from './(components)/sections/TestimonialsSection'; // Swiper might need this too if not careful
@@ -45,7 +23,7 @@ export default function HomePage() {
       <SkillsSection /> {/* If SkillsSection is safe or also needs dynamic loading, handle accordingly */}
       <ResumeSection />
       <ServicesSection />
-      <ContactSection />
+      <PortfolioSection />
 
       {/* Use the ClientSectionLoader for sections that need ssr:false */}
       {/* Pass props to tell it which sections to load, or modify it to always load them */}
@@ -54,6 +32,7 @@ export default function HomePage() {
         loadPortfolio={true}  // If PortfolioSection also needs ssr:false
         loadTestimonials={true} // If TestimonialsSection also needs ssr:false
       />
+      <ContactSection />
     </>
   );
 }
