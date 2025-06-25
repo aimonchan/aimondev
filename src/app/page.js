2d648fd144a334e543c995a7673883b7a95dcdb9
aimonchan <mr.minaimonchan@gmail.com>
2025-06-25 22:20:48 +0630

    Test4

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import HeroSection from './(components)/sections/HeroSection';
-// import AboutSection from './(components)/sections/AboutSection';
-// import StatsSection from './(components)/sections/StatsSection';
-// import SkillsSection from './(components)/sections/SkillsSection';
-// import ResumeSection from './(components)/sections/ResumeSection';
-// import PortfolioSection from './(components)/sections/PortfolioSection';
-// import ServicesSection from './(components)/sections/ServicesSection';
-// import TestimonialsSection from './(components)/sections/TestimonialsSection';
-// import ContactSection from './(components)/sections/ContactSection';
+import AboutSection from './(components)/sections/AboutSection';
+import StatsSection from './(components)/sections/StatsSection';
+import SkillsSection from './(components)/sections/SkillsSection';
+import ResumeSection from './(components)/sections/ResumeSection';
+import PortfolioSection from './(components)/sections/PortfolioSection';
+import ServicesSection from './(components)/sections/ServicesSection';
+import TestimonialsSection from './(components)/sections/TestimonialsSection';
+import ContactSection from './(components)/sections/ContactSection';
 
 // This is a Server Component by default, it just assembles other components.
 // The individual section components can be Client Components if they need interactivity.
